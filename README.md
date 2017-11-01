66cb2bad8344ddb43f4bcb7a679cb2718d0a0961
Alex Miller <asmiller1989@gmail.com>
2017-11-01 16:23:20 -0700

    Adds ability to scan with timestamps

--- README.md
+++ README.md
@@ -5,5 +5,13 @@ Given an address and a boundary of block numbers, look up the number of transact
 Example:
 
 ```
-node index.js --address=0x94dc1cf66c8fd62ef3bd7da53f47423862839823 --start=4472532 --end=4472534 --data 0x3290ce29
+node index.js --address=0x94dc1cf66c8fd62ef3bd7da53f47423862839823 --start_block=4472532 --end_block=4472534 --data 0x3290ce29
+```
+
+You may also search based on start and end timestamps. All timestamps are UNIX-formatted and in GMT. Note that this option is very slow and has to scan the entire blockchain in reverse until it finds appropriate anchor points (start and end blocks).
+
+Example with timestamps:
+
+```
+node index.js --address=0x94dc1cf66c8fd62ef3bd7da53f47423862839823 --start=1509578094 --end=1509578261 --data 0x3290ce29
 ```
--- index.js
+++ index.js
@@ -13,32 +13,61 @@ var web3 = new Web3(new Web3.providers.HttpProvider(PROVIDER_URL));
 // Optional flags
 var data = flags.get('data');
 var address = flags.get('address').toLowerCase();
-var start_block = Number(flags.get('start'));
-var end_block = Number(flags.get('end')) || start_block + 1;
-// Initialize the data store
-if (end_block < start_block || typeof end_block != 'number'
-  || typeof start_block != 'number' || !address) { console.log('Bad input'); }
-var store = Array.apply(null, Array(end_block-start_block));
+var start = Number(flags.get('start'));
+var end = Number(flags.get('end'));
+// Optional, but helpful
+var start_block = Number(flags.get('start_block'));
+var end_block = Number(flags.get('end_block'));
+
+if (!address) { console.log('Address is required!'); }
 
 // The CSV object to write (timestamp,count)
-var csv = 'timestamp,transactions\n';
+var csv_header = 'timestamp,transactions\n';
+var csv = '';
 
 // Run the script
 run();
 
 function run() {
+  if (!start_block || !end_block) {
+    getBlocks(function(blocks) {
+      start_block = blocks[0];
+      end_block = blocks[1];
+      console.log(`Found blocks: start=${start_block}, end=${end_block}`);
+      getCounts(function(_csv) { console.log(_csv); })
+    })
+  } else {
+    getCounts(function(_csv) { console.log(_csv); })
+  }
+}
+
+// Get a timeseries of counts
+// @returns string of form csv (e.g. `15356642345,1\n`)
+function getCounts(cb) {
+  var store = Array.apply(null, Array(end_block-start_block));
   var count = 0;
   store.forEach(function(s, i) {
     getCountInBlock(start_block+i, address, data, function(timestamp, hits) {
-      if (timestamp && count > 0) { csv += `${timestamp},${hits}\n`; }
+      if (timestamp && hits > 0) { csv += `${timestamp},${hits}\n`; }
       count += 1;
       if (count == store.length) {
-        console.log(csv);
+        if (csv == '') { cb('No transactions found.'); }
+        else { cb(csv_header+_csv)}
       }
     })
   })
 }
 
+// Get start and end blocks
+// @returns [start_block, end_block]
+function getBlocks(cb) {
+  console.log('Searching for start and end blocks...')
+  web3.eth.getBlockNumber(function(err, countdown) {
+    findStartEndBlock(start, end, countdown, null, function(blocks) {
+      cb(blocks);
+    })
+  })
+}
 
 // Get the number of transactions in a block
 // @returns (int, int)
@@ -48,7 +77,7 @@ function getCountInBlock(n, addr, data, cb) {
   var count = 0;
   // Get all of the transactions in a block
   web3.eth.getBlock(n, true, function(err, result) {
-    if (err) { reject(err); }
+    if (err) { console.log('error', err); }
     else {
       // Save the timestamp
       timestamp = result.timestamp;
@@ -71,3 +100,30 @@ function getCountInBlock(n, addr, data, cb) {
     };
   });
 }
+
+// Recursive search for start_block and end_block
+function findStartEndBlock(start, end, countdown, end_block, cb) {
+  var start_block = null;
+  getTimestampForBlock(countdown, function(timestamp) {
+    if (timestamp > end) { end_block = countdown; }
+    else if (!end_block && timestamp > end) {
+      console.log('Your end time is in the future!')
+      cb(null);
+    }
+    if (timestamp < start) { start_block = countdown; }
+    countdown -= 1;
+    if (start_block && end_block) {
+      cb([start_block, end_block]);
+    } else {
+      findStartEndBlock(start, end, countdown, end_block, cb);
+    }
+  })
+}
+
+// Search for a block by number and return the timestamp
+function getTimestampForBlock(n, cb) {
+  web3.eth.getBlock(n, true, function(err, result) {
+    if (err) { console.log('error', err); }
+    else { cb(result.timestamp); };
+  });
+};
